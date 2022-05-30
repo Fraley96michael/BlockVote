0c9049ffc33c5643fa8771d7a46b830d5a0e555f
Frale <fraley96michael@gmail.com>
2022-05-30 15:00:14 -0400

    make changes to the contract

--- src/App.js
+++ src/App.js
@@ -6,15 +6,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+// components
+import Home from "./Components/Home";
+import NewPoll from "./Components/NewPoll";
+import PollingStation from "./Components/PollingStation";
+
+// images
+import BlockVoteLogo from "./assets/blockvotelogo.svg";
+
 import getConfig from "./config";
 const { networkId } = getConfig(process.env.NODE_ENV || "development");
 
 export default function App() {
+  const changeCandidatesFunction = async (prompt) => {
+    console.log(prompt);
+    let namePair = await window.contract.getCandidatePair({ prompt: prompt });
+    localStorage.setItem("Candidate1", namePair[0]);
+    localStorage.setItem("Candidate2", namePair[1]);
+    localStorage.setItem("prompt", prompt);
+    window.location.replace(window.location.href + "PollingStation");
+  };
+
   return (
     <Router>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={BlockVoteLogo}></img>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mx-auto"></Nav>
@@ -27,6 +46,17 @@ export default function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <Switch>
+        <Route exact path="/">
+          <Home changeCandidates={changeCandidatesFunction} />
+        </Route>
+        <Route exact path="/PollingStation">
+          <PollingStation />
+        </Route>
+        <Route exact path="/NewPoll">
+          <NewPoll />
+        </Route>
+      </Switch>
     </Router>
   );
 }
